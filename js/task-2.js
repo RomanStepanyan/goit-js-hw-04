f55929f6e68de709111c705fc5ae350e6a761d77
RomanStepanyan <stepik10@gmail.com>
2020-07-31 13:56:17 +0300

    Second task is corrected

--- js/task-2.js
+++ js/task-2.js
@@ -17,14 +17,33 @@
     const actionInvertory = action.bind(inventory)
     actionInvertory(itemName);
   };
+
+  const invokeInventoryCall = function(itemName, action) {
+    console.log(`Invoking action on ${itemName}`);
+    
+    action.call(inventory, itemName);
+  };
+
+  const invokeInventoryApply = function(itemName, action) {
+    console.log(`Invoking action on ${itemName}`);
+    
+    action.apply(inventory, [itemName]);
+  };
+
   
   invokeInventoryAction('Medkit', inventory.add);
+  invokeInventoryCall('Medkit', inventory.add);
+  invokeInventoryApply('Medkit', inventory.add)
+
   // Invoking action on Medkit
   // Adding Medkit to inventory
   
   console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
   
   invokeInventoryAction('Gas mask', inventory.remove);
+  invokeInventoryCall('Gas mask', inventory.remove);
+  invokeInventoryApply('Gas mask', inventory.remove);
+
   // Invoking action on Gas mask
   // Removing Gas mask from inventory
   
